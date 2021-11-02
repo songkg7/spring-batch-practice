9c08846b927b54abcd94dbb802216c12d1830685
haril <songkg7@gmail.com>
2021-11-02 21:21:08 +0900

    JobParameter

--- src/main/java/com/inflearn/springbatchpractice/JobConfiguration.java
+++ src/main/java/com/inflearn/springbatchpractice/JobConfiguration.java
@@ -1,7 +1,9 @@
 package com.inflearn.springbatchpractice;
 
+import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
@@ -28,6 +30,18 @@ public class JobConfiguration {
     public Step step1() {
         return stepBuilderFactory.get("step1")
                 .tasklet((contribution, chunkContext) -> {
+
+                    JobParameters jobParameters = contribution.getStepExecution().getJobExecution()
+                            .getJobParameters();
+
+                    jobParameters.getString("name");
+                    jobParameters.getLong("seq");
+                    jobParameters.getDate("date");
+                    jobParameters.getDouble("age");
+
+                    Map<String, Object> jobParameters1 = chunkContext.getStepContext().getJobParameters();
+
+
                     System.out.println("step1 was execute!");
                     return RepeatStatus.FINISHED;
                 })
