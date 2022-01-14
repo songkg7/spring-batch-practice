b1b2a33456f12be980c626056366b600ef65092e
kgsong <kyunggeun.song@seavantage.com>
2022-01-14 23:39:03 +0900

    여러번 재시작을 가능하게 하는 incrementer

--- src/main/java/com/inflearn/springbatchpractice/JobConfig.java
+++ src/main/java/com/inflearn/springbatchpractice/JobConfig.java
@@ -5,9 +5,9 @@ import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
-import org.springframework.batch.core.job.DefaultJobParametersValidator;
 import org.springframework.batch.core.job.builder.FlowBuilder;
 import org.springframework.batch.core.job.flow.Flow;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -24,8 +24,7 @@ public class JobConfig {
                 .start(step1())
                 .next(step2())
                 .next(step3())
-                .validator(new DefaultJobParametersValidator(new String[]{"name", "date"}, new String[]{"count"}))
-                .preventRestart()
+                .incrementer(new RunIdIncrementer())
                 .build();
     }
 
