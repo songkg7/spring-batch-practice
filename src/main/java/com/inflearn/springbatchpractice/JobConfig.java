c87a8126bb8e053d863d19f33979c92bf47a4ec6
kgsong <kyunggeun.song@seavantage.com>
2022-01-15 23:47:08 +0900

    Tasklet

--- src/main/java/com/inflearn/springbatchpractice/JobConfig.java
+++ src/main/java/com/inflearn/springbatchpractice/JobConfig.java
@@ -1,15 +1,14 @@
 package com.inflearn.springbatchpractice;
 
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
-import org.springframework.batch.core.job.builder.FlowBuilder;
-import org.springframework.batch.core.job.flow.Flow;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.item.ItemProcessor;
-import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.batch.item.support.ListItemReader;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -23,21 +22,17 @@ public class JobConfig {
 
     @Bean
     public Job batchJob() {
-        return jobBuilderFactory.get("batchJob2")
+        return jobBuilderFactory.get("batchJob")
                 .incrementer(new RunIdIncrementer())
                 .start(step1())
                 .next(step2())
-                .next(step3())
                 .build();
     }
 
     @Bean
     public Step step1() {
         return stepBuilderFactory.get("step1")
-                .tasklet((contribution, chunkContext) -> {
-                    System.out.println("step1 has executed.");
-                    return RepeatStatus.FINISHED;
-                })
+                .tasklet(new CustomTasklet())
                 .build();
     }
 
@@ -45,52 +40,9 @@ public class JobConfig {
     public Step step2() {
         return stepBuilderFactory.get("step2")
                 .<String, String>chunk(chunkSize)
-                .reader(() -> null)
-                .processor((ItemProcessor<String, String>) s -> null)
-                .writer(list -> {
-
-                })
+                .reader(new ListItemReader<>(List.of("die", "aloud", "division", "inch", "rather")))
+                .processor((ItemProcessor<String, String>) String::toUpperCase)
+                .writer(items -> items.forEach(System.out::println))
                 .build();
-    }
-
-    @Bean
-    public Step step3() {
-        return stepBuilderFactory.get("step3")
-                .partitioner(step1())
-                .gridSize(2)
-                .build();
-    }
-
-    @Bean
-    public Step step4() {
-        return stepBuilderFactory.get("step4")
-                .job(job())
-                .build();
-    }
-
-    @Bean
-    public Job job() {
-        return jobBuilderFactory.get("job")
-                .start(step1())
-                .next(step2())
-                .next(step3())
-                .build();
-    }
-
-    @Bean
-    public Step step5() {
-        return stepBuilderFactory.get("step5")
-                .flow(flow())
-                .build();
-    }
-
-    @Bean
-    public Flow flow() {
-        FlowBuilder<Flow> flowBuilder = new FlowBuilder<>("flow");
-        flowBuilder.start(step3())
-                .next(step4())
-                .end();
-
-        return flowBuilder.build();
     }
 }
