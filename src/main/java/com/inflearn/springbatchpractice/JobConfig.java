4f132b0eab8aff176b8b5b82bcbcc4d7e6907414
haril <songkg7@gmail.com>
2022-01-16 22:45:11 +0900

    FlowJob - start(), next()

--- src/main/java/com/inflearn/springbatchpractice/JobConfig.java
+++ src/main/java/com/inflearn/springbatchpractice/JobConfig.java
@@ -6,8 +6,11 @@ import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.job.builder.FlowBuilder;
+import org.springframework.batch.core.job.flow.Flow;
 import org.springframework.batch.item.ItemProcessor;
 import org.springframework.batch.item.support.ListItemReader;
+import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -22,21 +25,28 @@ public class JobConfig {
     @Bean
     public Job batchJob() {
         return jobBuilderFactory.get("batchJob")
-                .start(step1())
-                .on("COMPLETED").to(step3())
-                .from(step1())
-                .on("FAILED").to(step2())
+                .start(flowA())
+                .next(step3())
+                .next(flowB())
+                .next(step6())
                 .end()
                 .build();
     }
 
     @Bean
+    public Flow flowA() {
+        FlowBuilder<Flow> flowBuilder = new FlowBuilder<>("flowA");
+        flowBuilder
+                .start(step1())
+                .next(step2())
+                .end();
+        return flowBuilder.build();
+    }
+
+    @Bean
     public Step step1() {
         return stepBuilderFactory.get("step1")
-                .tasklet((contribution, chunkContext) -> {
-                    throw new RuntimeException("step1 was failed.");
-//                    return RepeatStatus.FINISHED;
-                })
+                .tasklet((contribution, chunkContext) -> RepeatStatus.FINISHED)
                 .build();
     }
 
@@ -57,6 +67,37 @@ public class JobConfig {
                 .reader(new ListItemReader<>(List.of(377, 54, 960, 651, 274)))
                 .processor((ItemProcessor<Integer, Integer>) item -> 0)
                 .writer(items -> items.forEach(System.out::println))
+                .build();
+    }
+
+    @Bean
+    public Flow flowB() {
+        FlowBuilder<Flow> flowBuilder = new FlowBuilder<>("flowB");
+        flowBuilder
+                .start(step4())
+                .next(step5())
+                .end();
+        return flowBuilder.build();
+    }
+
+    @Bean
+    public Step step4() {
+        return stepBuilderFactory.get("step4")
+                .tasklet((contribution, chunkContext) -> RepeatStatus.FINISHED)
+                .build();
+    }
+
+    @Bean
+    public Step step5() {
+        return stepBuilderFactory.get("step5")
+                .tasklet((contribution, chunkContext) -> RepeatStatus.FINISHED)
+                .build();
+    }
+
+    @Bean
+    public Step step6() {
+        return stepBuilderFactory.get("step6")
+                .tasklet((contribution, chunkContext) -> RepeatStatus.FINISHED)
                 .build();
     }
 }
