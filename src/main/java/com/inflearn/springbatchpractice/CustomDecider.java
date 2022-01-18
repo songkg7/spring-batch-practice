c50ebb20aedff9dff89efaa9cd645efd6b6a819c
haril <songkg7@gmail.com>
2022-01-18 21:53:35 +0900

    JobExecutionDecider

---
+++ src/main/java/com/inflearn/springbatchpractice/CustomDecider.java
@@ -0,0 +1,20 @@
+package com.inflearn.springbatchpractice;
+
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.StepExecution;
+import org.springframework.batch.core.job.flow.FlowExecutionStatus;
+import org.springframework.batch.core.job.flow.JobExecutionDecider;
+
+public class CustomDecider implements JobExecutionDecider {
+
+    private int count = 1;
+
+    @Override
+    public FlowExecutionStatus decide(JobExecution jobExecution, StepExecution stepExecution) {
+        count++;
+        if (count % 2 == 0) {
+            return new FlowExecutionStatus("EVEN");
+        }
+        return new FlowExecutionStatus("ODD");
+    }
+}
--- src/main/java/com/inflearn/springbatchpractice/JobConfig.java
+++ src/main/java/com/inflearn/springbatchpractice/JobConfig.java
@@ -1,16 +1,12 @@
 package com.inflearn.springbatchpractice;
 
-import java.util.List;
 import lombok.RequiredArgsConstructor;
-import org.springframework.batch.core.ExitStatus;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
-import org.springframework.batch.core.job.builder.FlowBuilder;
-import org.springframework.batch.core.job.flow.Flow;
-import org.springframework.batch.item.ItemProcessor;
-import org.springframework.batch.item.support.ListItemReader;
+import org.springframework.batch.core.job.flow.JobExecutionDecider;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -21,86 +17,39 @@ public class JobConfig {
     private final JobBuilderFactory jobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;
 
-    private final int chunkSize = 10;
-
     @Bean
     public Job batchJob() {
         return jobBuilderFactory.get("batchJob")
+                .incrementer(new RunIdIncrementer())
                 .start(step1())
-                .on("FAILED").to(step2())
-                .on("PASS").stop()
+                .next(decider()).on("ODD").to(oddStep())
+                .from(decider()).on("EVEN").to(evenStep())
                 .end()
                 .build();
     }
 
     @Bean
-    public Flow flowA() {
-        FlowBuilder<Flow> flowBuilder = new FlowBuilder<>("flowA");
-        flowBuilder
-                .start(step1())
-                .next(step2())
-                .end();
-        return flowBuilder.build();
+    public JobExecutionDecider decider() {
+        return new CustomDecider();
     }
 
     @Bean
     public Step step1() {
         return stepBuilderFactory.get("step1")
-                .tasklet((contribution, chunkContext) -> {
-                    contribution.setExitStatus(ExitStatus.FAILED);
-                    return RepeatStatus.FINISHED;
-                })
-                .build();
-    }
-
-    @Bean
-    public Step step2() {
-        return stepBuilderFactory.get("step2")
-                .<String, String>chunk(chunkSize)
-                .reader(new ListItemReader<>(List.of("die", "aloud", "division", "inch", "rather")))
-                .processor((ItemProcessor<String, String>) String::toUpperCase)
-                .writer(items -> items.forEach(System.out::println))
-                .listener(new PassCheckingListener())
-                .build();
-    }
-
-    @Bean
-    public Step step3() {
-        return stepBuilderFactory.get("step3")
-                .<Integer, Integer>chunk(chunkSize)
-                .reader(new ListItemReader<>(List.of(377, 54, 960, 651, 274)))
-                .processor((ItemProcessor<Integer, Integer>) item -> 0)
-                .writer(items -> items.forEach(System.out::println))
-                .build();
-    }
-
-    @Bean
-    public Flow flowB() {
-        FlowBuilder<Flow> flowBuilder = new FlowBuilder<>("flowB");
-        flowBuilder
-                .start(step4())
-                .next(step5())
-                .end();
-        return flowBuilder.build();
-    }
-
-    @Bean
-    public Step step4() {
-        return stepBuilderFactory.get("step4")
                 .tasklet((contribution, chunkContext) -> RepeatStatus.FINISHED)
                 .build();
     }
 
     @Bean
-    public Step step5() {
-        return stepBuilderFactory.get("step5")
+    public Step oddStep() {
+        return stepBuilderFactory.get("oddStep")
                 .tasklet((contribution, chunkContext) -> RepeatStatus.FINISHED)
                 .build();
     }
 
     @Bean
-    public Step step6() {
-        return stepBuilderFactory.get("step6")
+    public Step evenStep() {
+        return stepBuilderFactory.get("evenStep")
                 .tasklet((contribution, chunkContext) -> RepeatStatus.FINISHED)
                 .build();
     }
