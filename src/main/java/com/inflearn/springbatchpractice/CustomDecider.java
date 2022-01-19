0639a126fb62c8ac1e007c7f2076a220cfeb8098
haril <songkg7@gmail.com>
2022-01-19 22:07:16 +0900

    JobExecutionDecider parameter 에 @NonNull 추가

--- src/main/java/com/inflearn/springbatchpractice/CustomDecider.java
+++ src/main/java/com/inflearn/springbatchpractice/CustomDecider.java
@@ -1,5 +1,6 @@
 package com.inflearn.springbatchpractice;
 
+import lombok.NonNull;
 import org.springframework.batch.core.JobExecution;
 import org.springframework.batch.core.StepExecution;
 import org.springframework.batch.core.job.flow.FlowExecutionStatus;
@@ -10,7 +11,7 @@ public class CustomDecider implements JobExecutionDecider {
     private int count = 1;
 
     @Override
-    public FlowExecutionStatus decide(JobExecution jobExecution, StepExecution stepExecution) {
+    public FlowExecutionStatus decide(@NonNull JobExecution jobExecution, StepExecution stepExecution) {
         count++;
         if (count % 2 == 0) {
             return new FlowExecutionStatus("EVEN");
