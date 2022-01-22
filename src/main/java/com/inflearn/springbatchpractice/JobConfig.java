8723b7d791f81d6aea9754671e6fc044d101559e
haril <songkg7@gmail.com>
2022-01-22 14:17:30 +0900

    Chunk

--- src/main/java/com/inflearn/springbatchpractice/JobConfig.java
+++ src/main/java/com/inflearn/springbatchpractice/JobConfig.java
@@ -1,5 +1,6 @@
 package com.inflearn.springbatchpractice;
 
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
@@ -7,8 +8,10 @@ import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.JobScope;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.support.ListItemReader;
 import org.springframework.batch.repeat.RepeatStatus;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -22,31 +25,36 @@ public class JobConfig {
     @Bean
     public Job batchJob() {
         return jobBuilderFactory.get("batchJob")
-                .start(step1(null))
-                .next(step3())
-                .listener(new CustomJobListener())
+                .start(step1())
+                .next(step2())
                 .build();
     }
 
     @Bean
     @JobScope
-    public Step step1(@Value("#{jobParameters['message']}") String message) {
-        log.info("message = {}", message);
+    public Step step1() {
         return stepBuilderFactory.get("step1")
-                .tasklet((contribution, chunkContext) -> RepeatStatus.FINISHED)
+                .<String, String>chunk(5)
+                .reader(new ListItemReader<>(List.of("anyone", "avenue", "praise", "unity", "double")))
+                .processor(new ItemProcessor<String, String>() {
+                    @Override
+                    public String process(String item) throws Exception {
+                        log.info("item = {}", item);
+                        return "my " + item;
+                    }
+                })
+                .writer(new ItemWriter<String>() {
+                    @Override
+                    public void write(List<? extends String> list) throws Exception {
+                        list.forEach(System.out::println);
+                    }
+                })
                 .build();
     }
 
     @Bean
     public Step step2() {
         return stepBuilderFactory.get("step2")
-                .tasklet((contribution, chunkContext) -> RepeatStatus.FINISHED)
-                .build();
-    }
-
-    @Bean
-    public Step step3() {
-        return stepBuilderFactory.get("step3")
                 .tasklet((contribution, chunkContext) -> RepeatStatus.FINISHED)
                 .build();
     }
