c781f87b7bb5375d9d6529e6564adfab425b139c
haril <songkg7@gmail.com>
2022-01-23 10:14:45 +0900

    ItemStream

--- src/main/java/com/inflearn/springbatchpractice/JobConfig.java
+++ src/main/java/com/inflearn/springbatchpractice/JobConfig.java
@@ -4,6 +4,8 @@ import com.inflearn.springbatchpractice.customer.CustomItemProcessor;
 import com.inflearn.springbatchpractice.customer.CustomItemReader;
 import com.inflearn.springbatchpractice.customer.CustomItemWriter;
 import com.inflearn.springbatchpractice.customer.Customer;
+import com.inflearn.springbatchpractice.customer.itemstream.CustomItemStreamReader;
+import com.inflearn.springbatchpractice.customer.itemstream.CustomItemStreamWriter;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -14,6 +16,8 @@ import org.springframework.batch.core.configuration.annotation.JobScope;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.item.ItemProcessor;
 import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.ItemStreamReader;
+import org.springframework.batch.item.ItemStreamWriter;
 import org.springframework.batch.item.ItemWriter;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
@@ -39,10 +43,20 @@ public class JobConfig {
     Step step1() {
         return stepBuilderFactory.get("step1")
                 .<Customer, Customer>chunk(3)
-                .reader(itemReader())
-                .processor(itemProcessor())
-                .writer(itemWriter())
+                .reader(itemStreamReader())
+                .writer(itemStreamWriter())
                 .build();
+    }
+
+    @Bean
+    ItemStreamReader<Customer> itemStreamReader() {
+        List<Customer> customers = List.of(Customer.of("user1"), Customer.of("user2"), Customer.of("user3"));
+        return new CustomItemStreamReader(customers);
+    }
+
+    @Bean
+    ItemStreamWriter<Customer> itemStreamWriter() {
+        return new CustomItemStreamWriter();
     }
 
     @Bean
