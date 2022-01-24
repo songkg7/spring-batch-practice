c93b2357560a06d70c29ce0757ca9ac5c45c35c5
haril <songkg7@gmail.com>
2022-01-24 22:08:42 +0900

    test: step unit test api

--- src/test/java/com/inflearn/springbatchpractice/JobConfigTest.java
+++ src/test/java/com/inflearn/springbatchpractice/JobConfigTest.java
@@ -3,13 +3,16 @@ package com.inflearn.springbatchpractice;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.time.LocalTime;
+import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.batch.core.BatchStatus;
 import org.springframework.batch.core.ExitStatus;
 import org.springframework.batch.core.JobExecution;
 import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.StepExecution;
 import org.springframework.batch.test.JobLauncherTestUtils;
 import org.springframework.batch.test.JobRepositoryTestUtils;
 import org.springframework.batch.test.context.SpringBatchTest;
@@ -47,5 +50,25 @@ class JobConfigTest {
         // then
         assertEquals(BatchStatus.COMPLETED, jobExecution.getStatus());
         assertEquals(ExitStatus.COMPLETED, jobExecution.getExitStatus());
+    }
+
+    @Test
+    @DisplayName("step1 unit test")
+    void test_2() throws Exception {
+        // given
+        JobParameters jobParameters = new JobParametersBuilder()
+                .addString("name", "user1")
+                .addLong("date", LocalTime.now().toNanoOfDay())
+                .toJobParameters();
+
+        // when
+        JobExecution jobExecution = jobLauncherTestUtils.launchStep("step1");
+
+        // then
+        List<StepExecution> stepExecutions = (List<StepExecution>) jobExecution.getStepExecutions();
+        StepExecution stepExecution = stepExecutions.get(0);
+        stepExecution.getCommitCount();
+        stepExecution.getReadCount();
+        stepExecution.getWriteCount();
     }
 }
