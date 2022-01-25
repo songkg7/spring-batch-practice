b0a0ff909312390d19ca1eca875ad58ff027c133
haril <songkg7@gmail.com>
2022-01-25 22:04:07 +0900

    RepeatTemplate ExceptionHandler

--- src/main/java/com/inflearn/springbatchpractice/JobConfig.java
+++ src/main/java/com/inflearn/springbatchpractice/JobConfig.java
@@ -14,11 +14,8 @@ import org.springframework.batch.item.ItemReader;
 import org.springframework.batch.item.NonTransientResourceException;
 import org.springframework.batch.item.ParseException;
 import org.springframework.batch.item.UnexpectedInputException;
-import org.springframework.batch.repeat.CompletionPolicy;
-import org.springframework.batch.repeat.RepeatStatus;
-import org.springframework.batch.repeat.policy.CompositeCompletionPolicy;
-import org.springframework.batch.repeat.policy.SimpleCompletionPolicy;
-import org.springframework.batch.repeat.policy.TimeoutTerminationPolicy;
+import org.springframework.batch.repeat.exception.ExceptionHandler;
+import org.springframework.batch.repeat.exception.SimpleLimitExceptionHandler;
 import org.springframework.batch.repeat.support.RepeatTemplate;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -56,22 +53,12 @@ public class JobConfig {
                     RepeatTemplate repeatTemplate = new RepeatTemplate();
                     @Override
                     public String process(String item) throws Exception {
-
-                        // 종료 정책은 일반적으론 2가지 이상 설정할 수 없다.
-                        repeatTemplate.setCompletionPolicy(new SimpleCompletionPolicy(4));
-                        repeatTemplate.setCompletionPolicy(new TimeoutTerminationPolicy(3000));
-
-                        // 여러 정책을 복합적으로 사용하기 위해서 다음 클래스를 사용한다.
-                        CompositeCompletionPolicy compositeCompletionPolicy = new CompositeCompletionPolicy();
-                        CompletionPolicy[] completionPolicies = new CompletionPolicy[]{
-                                new SimpleCompletionPolicy(3), new TimeoutTerminationPolicy(3000) };
-
-                        compositeCompletionPolicy.setPolicies(completionPolicies);
-                        repeatTemplate.setCompletionPolicy(compositeCompletionPolicy);
+                        repeatTemplate.setExceptionHandler(simpleLimitExceptionHandler());
 
                         repeatTemplate.iterate(context -> {
                             log.info("repeatTemplate is testing");
-                            return RepeatStatus.CONTINUABLE;
+                            throw new RuntimeException("Exception is occurred");
+//                            return RepeatStatus.CONTINUABLE;
                         });
 
                         return item;
@@ -79,5 +66,10 @@ public class JobConfig {
                 })
                 .writer(items -> items.forEach(item -> log.info("item = {}", item)))
                 .build();
+    }
+
+    @Bean
+    ExceptionHandler simpleLimitExceptionHandler() {
+        return new SimpleLimitExceptionHandler(3);
     }
 }
